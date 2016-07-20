b0c957d230aa2a71535f5ae176facc9b7a4d2bf6
luoziyihao <wangyiraoxiang@163.com>
2016-07-20 22:06:26 +0800

    com

--- set_env.sh
+++ set_env.sh
@@ -48,7 +48,7 @@ ln -fs ${dircolors_solarized}"/dircolors.ansi-light" ${dircolors_solarized_dest}
 ln -fs ${DOTFILES}"/.bashrc" ${MY_HOME}"/.bashrc"
 ln -fs ${DOTFILES}"/.tmux.conf" ${MY_HOME}"/.tmux.conf"
 ln -fs ${DOTFILES}"/.vimrc" ${MY_HOME}"/.vimrc"
-ln -fs ${DOTFILES}"/.ycm_extra_conf" ${MY_HOME}"/.ycm_extra_conf"
+ln -fs ${DOTFILES}"/.ycm_extra_conf.py" ${MY_HOME}"/.ycm_extra_conf.py"
 ln -fs ${DOTFILES}"/.zshrc" ${MY_HOME}"/.zshrc"
 ln -fs ${oh_my_zsh} ${oh_my_zsh_dest}
 
