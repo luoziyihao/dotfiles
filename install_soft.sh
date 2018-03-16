2f1e4bc6469f2c38ad7035a87dd086e345f94631
xiang.rao <wangyiraoxiang@163.com>
2018-03-16 01:08:35 -0400

    force push

--- install_soft.sh
+++ install_soft.sh
@@ -7,7 +7,10 @@
 #########################################################################
 
 sudo apt-get update
+sudo apt-get install git
 sudo apt-get install chromium-browser 
 sudo apt-get vim
 sudo apt-get python-pip
 sudo pip install shadowsocks
+sudo pip shutter
+curl -L https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
