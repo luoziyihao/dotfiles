f1e5c3567537fa4ebacf06b2a406ef127d4f1173
xiang.rao <wangyiraoxiang@163.com>
2018-04-07 20:35:52 +0800

    force push

--- create_workspace.sh
+++ create_workspace.sh
@@ -1,31 +1,35 @@
-sudo chown -R ` who |head -n 1|awk '{print($1)}'` /usr/local
+
 mkdir -p ~/Works/MY_PROJECT
 mkdir -p ~/Works/OPEN_SOURCE
 mkdir -p ~/Works/Qunar
 mkdir -p ~/Works/hk
 mkdir -p ~/WorkTemp
+sudo mkdir -p /data
 
+# 权限
+sudo chown -R luoziyihao /data/
+sudo chown -R ` who |head -n 1|awk '{print($1)}'` /usr/local
+sudo chown -R luoziyihao:root  /usr/local/share/zsh
+sudo chmod  -R 755  /usr/local/share/zsh
+
+
+# cmd
 cd ~/Works/MY_PROJECT/
-
 git clone https://github.com/luoziyihao/shellManage.git 
 cd ./shellManage
- ln -fs $(pwd)/p /usr/local/bin/por
- ln -fs $(pwd)/r /usr/local/bin/rer
- ln -fs $(pwd)/kills.sh /usr/local/bin/ks
+ln -fs $(pwd)/p /usr/local/bin/por
+ln -fs $(pwd)/r /usr/local/bin/rer
+ln -fs $(pwd)/kills.sh /usr/local/bin/ks
 
+# vpn
 alias vpncc="sudo vpnc-connect"
 alias fcps="gaa && gcmsg 'force push' &&ggpush"
 
+# git
 git config --global user.email "wangyiraoxiang@163.com"
 git config --global user.name "xiang.rao"
 
 # pyenv
+sudo apt-get install python-pip
 cd
 curl -L https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-
-# zsh 权限
-sudo chown -R luoziyihao:root  /usr/local/share/zsh
-sudo chmod  -R 755  /usr/local/share/zsh
-
-# data
- sudo chown -R luoziyihao /data/
