8215279d7e60cf083b25ee46b2b103ef40822a65
xiang.rao <wangyiraoxiang@163.com>
2018-05-11 23:13:41 +0800

    force push

--- create_workspace.sh
+++ create_workspace.sh
@@ -7,15 +7,23 @@ mkdir -p ~/WorkTemp
 sudo mkdir -p /data
 
 # 权限
-sudo chown -R luoziyihao /data/
-sudo chown -R ` who |head -n 1|awk '{print($1)}'` /usr/local
-sudo chown -R luoziyihao:root  /usr/local/share/zsh
+sudo chown -R $(whoami) /data/
+sudo chown -R  $(whoami) /usr/local
+sudo chown -R  $(whoami):root  /usr/local/share/zsh
 sudo chmod  -R 755  /usr/local/share/zsh
 
 
 # cmd
 cd ~/Works/MY_PROJECT/
-git clone https://github.com/luoziyihao/shellManage.git 
+shellManage=${HOME}/Works/MY_PROJECT/shellManage
+if [ -d "${shellManage}" ]; then
+    echo -e "\033[34m archive: ${shellManage} \033[0m"
+else 
+    echo -e "\033[31m archive '${shellManage}' not exists, just git clone it \033[0m"
+    git clone git@github.com:luoziyihao/shellManage.git
+fi
+
+
 cd ./shellManage
 ln -fs $(pwd)/p /usr/local/bin/por
 ln -fs $(pwd)/r /usr/local/bin/rer
@@ -38,13 +46,15 @@ curl -L https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer |
 
 
 # install
+sudo apt-get install tmux
 sudo apt-get install aptitude
 sudo apt-get install shutter
 sudo apt-get install vim-gnome
-#sudo apt-get install chromium-browser
-sudo apt-get install nautilus-open-terminal
-# sudo apt-get install uget
-# sudo apt-get install aria2
+sudo apt-get install chromium-browser
+# comment for ubuntu 18.04
+# sudo apt-get install nautilus-open-terminal
+sudo apt-get install uget
+sudo apt-get install aria2
 sudo apt-get install ibus ibus-clutter ibus-gtk ibus-gtk3 ibus-qt4
 sudo apt-get install  ibus-sunpinyin
 sudo apt-get install ibus-table-emoji
